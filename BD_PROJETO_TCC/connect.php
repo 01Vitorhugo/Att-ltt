79c1cc17b37e4c121018984775e1cf1ae368129c
Vitor Hugo <vitorhugo0031@outlook.com>
2023-03-30 15:56:15 -0300

    Add conteudo para o portifolio

--- BD_PROJETO_TCC/connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $con = mysqli_connect('localhost','root','','bd_buscampo');
-    mysqli_query($con,"SET NAMES utf8");
-?>
--- BD_PROJETO_TCC/logoff.php
+++
@@ -1,8 +0,0 @@
-<?php
-@session_start();
-unset($_SESSION['login']);
-unset($_SESSION['nome']);
-unset($_SESSION['id']);
-$login['nome'] = "";
-header("location:login.php");
-?>
